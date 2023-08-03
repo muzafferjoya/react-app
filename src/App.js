2b8d022e34648a89638fc38968717b9ebca4ec0c
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2023-08-03 17:53:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-          Edit <code>src/App.js</code> and save to reload deploy on S3
+          Edit <code>src/App.js</code> and save to reload deploy on docker container
         </p>
         
         <p> Welcome </p>
