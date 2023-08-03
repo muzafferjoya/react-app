8449e0fb56c73d7555bcdc3f2fc67835d7cecd8e
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2023-08-03 18:06:03 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo ">>>>>>>>>>>>>> CD to docker build folder and Recreate nestjs docker service:"
-cd /home/ubuntu/reactjs/ && docker-compose up -d --no-deps --force-recreate reactjs
