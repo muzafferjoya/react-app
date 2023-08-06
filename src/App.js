c2baf464c77c17a37b065de861f8ff3edb4f8f0f
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2023-08-06 12:41:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
 
           Edit <code>src/App.js</code> CI/CD Successfully running on docker contaner
@@ -13,14 +13,6 @@ function App() {
         
         <p> Welcome </p>
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
